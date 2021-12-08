b6e84a52260166f8505a64759f8d65ea3e69b037
noahl <noahl@Noah>
2021-12-08 16:15:01 +0530

    commit 8

---
+++ crm_proj/src/main/java/Generic_Utilities/Retry_Analyzer.java
@@ -0,0 +1,20 @@
+package Generic_Utilities;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class Retry_Analyzer implements IRetryAnalyzer {
+  private int count=0;
+  private int retry_count=3;
+	public boolean retry(ITestResult result) {
+		while(count<retry_count)
+		{
+			count++;
+			return true;
+		}
+
+		return false;
+	}
+
+	
+}
--- crm_proj/src/test/java/crm_proj/leads1.java
+++ crm_proj/src/test/java/crm_proj/leads1.java
@@ -1,12 +1,20 @@
 package crm_proj;
 
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
-//@Listeners(Generic_Utilities.ITestList.class)
+
+
+@Listeners(Generic_Utilities.ITestList.class)
+
 public class leads1 extends Base_class {
 
-	@Test (groups = {"RegionalTest"})
+	
+	@Test(retryAnalyzer = Generic_Utilities.Retry_Analyzer.class)
+	
+	
+	//@Test (groups = {"RegionalTest"})
 	public void tcl_1()
 	{
 		
@@ -19,6 +27,7 @@ public class leads1 extends Base_class {
 		driver.findElement(By.name("secondaryemail")).sendKeys("noahlenin19@gmail.com");
 		driver.findElement(By.id("email")).sendKeys("noahlenin19@gmail.com");
 		driver.findElement(By.xpath("//input[@class='crmbutton small save']")).click();
+		Assert.assertTrue(false);
 	}
 	
 }
